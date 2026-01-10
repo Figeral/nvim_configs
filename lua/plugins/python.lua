52dbc1a5c4139b54683b123830cea701d47d9862
Figeral <mouliofitzgerard@gmail.com>
2026-01-11 00:47:13 +0100

    feat : adding support to python

---
+++ lua/plugins/python.lua
@@ -0,0 +1,40 @@
+return {
+  -- 1. Python LSP (Existing)
+  -- Provides "Hover" documentation (press 'K' on a symbol)
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        pyright = {},
+      },
+    },
+  },
+
+  -- 2. Python Debugger (Existing)
+  {
+    "mfussenegger/nvim-dap",
+    dependencies = {
+      "mfussenegger/nvim-dap-python",
+      "rcarriga/nvim-dap-ui",
+    },
+    config = function()
+      -- Ensure you have debugpy installed (e.g., via Mason or pip)
+      require("dap-python").setup("python") 
+    end,
+  },
+
+
+
+  -- 3. Generate Docstrings (NEW)
+  -- Generates Google/Numpy style docstrings automatically
+  {
+    "danymat/neogen",
+    dependencies = "nvim-treesitter/nvim-treesitter",
+    config = true,
+    -- Uncomment to map a key to generate docstrings:
+    -- keys = {
+    --   { "<leader>nf", function() require("neogen").generate() end, desc = "Generate Docstring" },
+    -- },
+  },
+}
+
